8cec116ba38d895a39517d3ec7acbbcacd2aff71
Flávio Henrique <flavio.souza99@etec.sp.gov.br>
2024-08-22 22:24:24 -0300

    Update jogodaforca.py

--- jogodaforca.py
+++ jogodaforca.py
@@ -1,37 +1,35 @@
-palavra = "python"
-letras_usuario = []
-chances = 4
+chances = 6
+letrasJogadas = []
 ganhou = False
-while True:
-    # criar a nossa logica
-    for letra in palavra:
-        if letra.upper() in letras_usuario:
-            print(letra, end=" ")
-        else:
-            print("_", end=" ")
 
-    print(f"Você tem {chances} chances")
+resposta = input("Digite a resposta: ").upper().split()[:1:][0]  # Retorna a primeira palavra em string
+letraResposta = list(resposta)  # Retorna um array com cada letra separada
+quantLetra = len(resposta)
+letrasAcertadas = ['_' for _ in range(quantLetra)]  # Lista para armazenar letras acertadas
 
-    tentativa = input("Escolha uma letra para adivinhar: ")
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
-    letras_usuario.append(tentativa.upper())
+while not ganhou and chances > 0:
+    print("Palavra: ", ' '.join(letrasAcertadas))
+    pergunta = input("Digite uma letra: ").upper()
     
-    if tentativa.upper() not in palavra.upper():
-        chances -= 1
-    if tentativa.upper() in palavra.upper() == palavra.upper():
-        print("Essa letra já foi digitada.")
-        chances -= 1
+    if pergunta in letrasJogadas:
+        print("\nVocê já jogou essa letra. Tente outra.")
+        continue
+    
+    letrasJogadas.append(pergunta)
+    
+    if pergunta in letraResposta:
+        print("\nAcertou!")
+        for i in range(quantLetra):
+            if letraResposta[i] == pergunta:
+                letrasAcertadas[i] = pergunta
     else:
+        print(f"\nErrou! Chances restantes: {chances}")
+        chances -= 1
+    
+    if '_' not in letrasAcertadas:
         ganhou = True
-    
-    for letra in palavra:
-        if letra.upper() not in letras_usuario:
-            ganhou = False
-    
-    if chances == 0 or ganhou:
-        break
-
-if ganhou:
-    print(f"Parabéns, você ganhou. A palavra era: {palavra}")
-else:
-    print(f"Você perdeu! A palavra era: {palavra}")
+        print("Parabéns! Você ganhou!")
+    elif chances == 0:
+        print(f"Você perdeu! A palavra era: {resposta.capitalize()}")
